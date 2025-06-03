70a3b77bdc83fa6c962febad7015e30ce234bf82
Aky0905 <wonder090517@gmail.com>
2025-06-03 12:03:27 +0900

    yammy

--- products/forms.py
+++ products/forms.py
@@ -4,4 +4,14 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['title', 'description', 'price', 'category', 'image']
+        fields = ['image', 'title', 'category', 'price', 'description']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': '제품명을 입력하세요'}),
+            'price': forms.NumberInput(attrs={'placeholder': '가격을 입력하세요'}),
+            'description': forms.Textarea(attrs={
+                'placeholder': '제품 설명을 입력하세요',
+                'rows': 6,      # 원하는 행 개수로 늘려주세요
+                'cols': 40,     # 필요시 열 개수도 지정
+                # 'style': 'resize: both;', # 크기 조절 핸들 나오게 하고 싶으면 추가
+            }),
+        }
